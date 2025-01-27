0973fb76dc5bbc4c5ebe4525330b69df3382e92b
giuseppebrandi-byui <bra22041@byui.edu>
2025-01-27 22:34:44 +0000

    Added authentication feature

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 
+const passport = require('passport');
+
+
 router.use('/', require('./swagger'));
-
-router.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 router.use('/products', require('./products'));
 router.use('/customers', require('./customers'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => { });
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const { productValidationRules, validate } = require('./validation-products');
 const productsController = require('../controllers/products');
+const {isAuthenticated} = require('./authenticate');
 
 router.get('/', productsController.getAll);
 
 router.get('/:id', productsController.getSingle);
 
-router.post('/', productValidationRules(), validate, productsController.createProduct);
+router.post('/', productValidationRules(), validate, isAuthenticated, productsController.createProduct);
 
-router.put('/:id', productValidationRules(), validate, productsController.updateProduct);
+router.put('/:id', productValidationRules(), validate, isAuthenticated, productsController.updateProduct);
 
-router.delete('/:id', productsController.deleteProduct);
+router.delete('/:id', isAuthenticated, productsController.deleteProduct);
 
 module.exports = router;
