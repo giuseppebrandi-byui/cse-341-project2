2adb2b10185d86d8a6dd912ae3e85a967c094a52
giuseppebrandi-byui <bra22041@byui.edu>
2025-01-29 18:54:43 +0000

    Debugging displayName

--- server.js
+++ server.js
@@ -51,7 +51,12 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out') });
+app.get('/', (req, res) => {
+  console.log(req.session);
+  console.log(req.session.user);
+  alert(req.session.user.toString());
+  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')
+});
 
 app.get('/github/callback', passport.authenticate('github', {
   failureRedirect: '/api-docs', session: false
