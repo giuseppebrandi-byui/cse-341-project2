1399df92e1cb392648d7cef8b31b9353abee6be2
giuseppebrandi-byui <bra22041@byui.edu>
2025-01-29 19:01:47 +0000

    Solve null issue with displayName

--- server.js
+++ server.js
@@ -52,10 +52,7 @@ passport.deserializeUser((user, done) => {
 });
 
 app.get('/', (req, res) => {
-  console.log(req.session);
-  console.log(req.session.user);
-  alert(req.session.user.toString());
-  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')
+  res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName??req.session.user.username}` : 'Logged Out')
 });
 
 app.get('/github/callback', passport.authenticate('github', {
