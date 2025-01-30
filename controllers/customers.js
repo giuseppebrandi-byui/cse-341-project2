cea65147b54f1903496f83b2226b7c9be370058d
giuseppebrandi-byui <bra22041@byui.edu>
2025-01-30 10:04:55 +0000

    Add swagger comments for each status code

--- controllers/customers.js
+++ controllers/customers.js
@@ -6,24 +6,27 @@ const getAll = async (req, res, next) => {
   try {
     const customers_result = await mongodb.getDatabase().db().collection('customers').find();
     customers_result.toArray().then((customers) => {
-      // If the function returns an empty array, return 404 status code
+      // If the function returns an empty array, return 400 status code
       if (customers.length === 0 || !customers) {
-        // #swagger.responses[400] = No customers found
+        // #swagger.responses[400] = 'No customers found'
         next(createError(400, 'No customers found'));
         return;
       }
       res.setHeader('Content-Type', 'application/json');
-      // #swagger.responses[200] = No customers found
+      // #swagger.responses[200] = 'customers found'
       res.status(200).json(customers);
     });
   } catch (error) { 
+    // #swagger.responses[500] = 'Something went wrong. Try again later.'
     next(createError(500, 'Something went wrong. Try again later.'));
     return;
   }
 }
 
 const getSingle = async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
   if (!(req.params.id && req.params.id.length === 24)) {
+    // #swagger.responses[400] = 'Please enter a valid id with a string of 24 hex characters!'
     next(createError(400, 'Please enter a valid id with a string of 24 hex characters!'));
     return;
   }
@@ -31,14 +34,17 @@ const getSingle = async (req, res, next) => {
     const customerId = ObjectId.createFromHexString(req.params.id);
     const customers_result = await mongodb.getDatabase().db().collection('customers').find({_id: customerId});
     customers_result.toArray().then((customers) => {
-      if (customers.length === 0 || !customers) { 
-        next(createError(404, 'Sorry! No customer with the entered id '));
+      if (customers.length === 0 || !customers) {
+        // #swagger.responses[404] = 'Sorry! No customer with the entered id.'
+        next(createError(404, 'Sorry! No customer with the entered id'));
         return;
     }
       res.setHeader('Content-Type', 'application/json');
+      // #swagger.responses[200] = 'customer found'
       res.status(200).json(customers[0]);
     });
   } catch (error) { 
+    // #swagger.responses[500] = 'Something went wrong. Try again later.'
     createError(500, 'Something went wrong. Try again later.');
   }
 }
@@ -81,7 +87,7 @@ const createCustomer = async (req, res, next) => {
       return;
     }
   } catch (error) { 
-    // #swagger.responses[400] =  'Something went wrong. Try later.'
+    // #swagger.responses[500] =  'Something went wrong. Try later.'
     next(createError(500, 'Something went wrong. Try later.'));
   }
 }
@@ -110,15 +116,18 @@ const updateCustomer = async (req, res, next) => {
     };
     const response = await mongodb.getDatabase().db().collection('customers').replaceOne({ _id: customerId }, customer);
     if (response.modifiedCount > 0) {
+      // #swagger.responses[201] = { 'message: ': 'Your customer has been updated successfully.','Updated Customer: ' : 'customer'}
       res.status(201).json({
         'Message: ': 'Your customer has been updated successfully',
         'Updated Customer: ': customer
       });
     } else {
+      // #swagger.responses[400] =  'Sorry no customer with that id.'
       next(createError(400, 'Sorry no customer with that id'));
       return;
     }
-  } catch (error) { 
+  } catch (error) {
+    // #swagger.responses[500] =  'Something went wrong while updating the customer. Please check id.'
     next(createError(500, 'Something went wrong while updating the customer. Please check id.'));
   }
 }
@@ -127,20 +136,24 @@ const deleteCustomer = async (req, res, next) => {
   res.setHeader('Content-Type', 'application/json')
   try {
     if (!(req.params.id && req.params.id.length === 24)) {
+    // #swagger.responses[400] = 'Please enter a valid id with a string of 24 hex characters!'
       next(createError(400, 'Please enter a valid id with a string of 24 hex characters!'));
       return;
     }
     const customerId = ObjectId.createFromHexString(req.params.id);
     const response = await mongodb.getDatabase().db().collection('customers').deleteOne({ _id: customerId }, true);
     if (response.deletedCount > 0) {
+      // #swagger.responses[201] = { 'message: ': 'The customer has been deleted successfully.'}
       res.status(201).json({
         'Message: ': 'The customer has been deleted successfully.',
       });
     } else {
+      // #swagger.responses[400] = 'Sorry no customer with that id.'
       next(createError(400, 'Sorry no customer with that id.'));
       return;
     };
-  } catch (error) { 
+  } catch (error) {
+    // #swagger.responses[500] = 'Something went wrong while deleting the customer.'
     next(createError(500, 'Something went wrong while deleting the customer.'));
   }
 }
